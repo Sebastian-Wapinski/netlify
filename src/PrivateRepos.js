e418d1c267c40c8bb4280f95484f1face30ff7f0
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-02 15:17:37 +0100

    changes

---
+++ src/PrivateRepos.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+async function getRepos(filter) {
+  const repos = await fetch('/api/getPrivateRepos')
+    .then(resp => resp.json());
+  return repos.filter(
+    repo => repo.name.indexOf(filter) === 0);
+}
+
+export default function PrivateRepos() {
+  const [reposList, setRepoList] = useState([]);
+
+  useEffect(() => {
+    getRepos('task-html').then(
+      data => setRepoList(data));
+  }, []);
+
+  return (
+    <ul>
+      {reposList.map(repo => <li>{repo.name}</li>)}
+    </ul>
+  );
+}
